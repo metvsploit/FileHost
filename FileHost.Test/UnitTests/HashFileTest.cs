dd70472410f7adf864098f1e5a4d5e8605cf8d9a
metvsploit <dkwathout@yandex.ru>
2022-09-12 20:32:43 +0500

    Модульное тестирование класса HashFile(для назначения идентификатора файла)

---
+++ FileHost.Test/UnitTests/HashFileTest.cs
@@ -0,0 +1,30 @@
+﻿using FileHost.WebApi.Entities;
+using FileHost.WebApi.Helpers;
+using Xunit;
+
+namespace FileHost.Test.UnitTests
+{
+    public class HashFileTest
+    {
+        [Fact]
+        public void Successfull_Hashing_File()
+        {
+            //Arrange
+            var model = new FileData
+            {
+                FileName = "test",
+                Format = "test",
+                Source = "test",
+                Data = new byte[] { 74, 65, 73, 74 }
+            };
+
+            //Act
+            var hash = HashFile.GetHashString(model.FileName + model.Format + model.Source +
+                System.Text.Encoding.UTF8.GetString(model.Data));
+
+            //Assert
+            Assert.NotNull(hash);
+            Assert.Equal(6, hash.Length);
+        }
+    }
+}
