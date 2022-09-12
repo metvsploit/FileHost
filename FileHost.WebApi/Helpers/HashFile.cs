f4c79325f8520266d8e1d18aafac2a006678559b
metvsploit <dkwathout@yandex.ru>
2022-09-12 20:10:37 +0500

    Добавлен класс - задает уникальный идентификатор для файла

---
+++ FileHost.WebApi/Helpers/HashFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FileHost.WebApi.Helpers
+{
+    public static class HashFile
+    {
+        private static byte[] GetHash(string input)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+        }
+
+        public static string GetHashString(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(input))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString().Substring(0, 6);
+        }
+    }
+}
