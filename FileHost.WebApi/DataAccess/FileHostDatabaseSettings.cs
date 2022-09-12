8667d9ef88b785071782554e3140f82ef55ef452
metvsploit <dkwathout@yandex.ru>
2022-09-12 20:06:14 +0500

    Добавлен класс - настройка подключения к базе данных MongoDb

---
+++ FileHost.WebApi/DataAccess/FileHostDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FileHost.WebApi.DataAccess
+{
+    public class FileHostDatabaseSettings
+    {
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+        public string? CollectionName { get; set; }
+    }
+}
