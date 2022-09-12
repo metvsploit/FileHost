c8ef8c9c653cfced36d89207008a6e3468b161a8
metvsploit <dkwathout@yandex.ru>
2022-09-12 20:14:52 +0500

    Добавлен FileService, FileHostDatabase в контейнер Добавлено разрешение для обработки запросов с  адреса localhost:3000

--- FileHost.WebApi/Program.cs
+++ FileHost.WebApi/Program.cs
@@ -1,6 +1,25 @@
+using FileHost.WebApi.DataAccess;
+using FileHost.WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<FileHostDatabaseSettings>(
+    builder.Configuration.GetSection("FileHostDatabase"));
+
+builder.Services.AddSingleton<FileService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +37,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
