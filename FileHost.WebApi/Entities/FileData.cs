cd79d4a4c1172070cd71fd3ba30bdc64c6d6e42e
metvsploit <dkwathout@yandex.ru>
2022-09-12 20:00:11 +0500

    Добавлен класс - описание загружаемого файла

---
+++ FileHost.WebApi/Entities/FileData.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FileHost.WebApi.Entities
+{
+    public class FileData
+    {
+        [BsonId]
+        public string? Id { get; set; }
+        public string? FileName { get; set; }
+        public long? Size { get; set; }
+        public string? Format { get; set; }
+        public string? Source { get; set; }
+        public byte[]? Data { get; set; }
+        public DateTime? StorageTime { get; set; }
+    }
+}
